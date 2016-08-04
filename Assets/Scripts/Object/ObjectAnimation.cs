ddf3de439be1a8db6252f6382158ca6892d7c90b
Max Harlynking <mharlynk@andrew.cmu.edu>
2016-08-04 02:19:25 -0700

    Got to level 25

---
+++ Assets/Scripts/Object/ObjectAnimation.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectAnimation : MonoBehaviour {
+
+	public Vector2 destination;
+	public float speed = 3f;
+	private Vector2 start;
+	private Vector2 end;
+
+	void Start () {
+		start = (Vector2)transform.position;
+		end = (Vector2)transform.position + destination;
+		ResetAnim(start, end);
+	}
+
+	IEnumerator SmoothLerpLoop(Vector2 start, Vector2 end) {
+		float t = 0f;
+		while (t <= 1f){
+			t += Time.deltaTime/speed;
+			transform.position = Vector2.Lerp(start, end, Mathf.SmoothStep(0f, 1f, t));
+			yield return null;
+		}
+		ResetAnim(end, start);
+	}
+
+	void ResetAnim (Vector2 start, Vector2 end) {
+		StartCoroutine(SmoothLerpLoop(start, end));
+	}
+
+	void OnDrawGizmosSelected () {
+		Gizmos.color = Color.gray;
+		Vector2 drawDestination = (Vector2)transform.position + destination;
+		Gizmos.DrawWireSphere(drawDestination, 0.2f);
+		Gizmos.DrawLine(transform.position, drawDestination);
+	}
+}
