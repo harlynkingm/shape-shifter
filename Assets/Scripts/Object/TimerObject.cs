a7285c2162a19816a29d1a4eae2e4e390f21e276
Max Harlynking <mharlynk@andrew.cmu.edu>
2016-08-14 16:47:26 -0700

    Fade out, again

---
+++ Assets/Scripts/Object/TimerObject.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerObject : MonoBehaviour {
+
+	public SpriteRenderer[] countdown;
+	public float delay = 1f;
+	public float waitTime = 2f;
+	public bool disappear = true;
+	public GameObject trigger;
+
+	private float grey = 0.7f;
+
+	private int curTime = 0;
+
+	void Start () {
+		Invoke("AddTime", delay);
+		trigger.transform.parent = null;
+		for (int i = 0; i < countdown.Length; i++){
+			countdown[i].color = new Color(grey, grey, grey, 1f);
+		}
+	}
+
+	void AddTime () {
+		if (curTime < countdown.Length){
+			countdown[curTime].color = new Color(grey, grey, grey, 0f);
+		}
+		curTime++;
+		if (curTime > countdown.Length){
+			DoAction();
+		} else {
+			Invoke("AddTime", 1f);
+		}
+	}
+
+	void DoAction () {
+		if (disappear){
+			StartCoroutine(Disappear());
+		}
+	}
+
+	IEnumerator Disappear(){
+		GetComponent<Collider2D>().enabled = false;
+		SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+		while (transform.localScale.x > 0.665f){
+			transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one * 0.66f, Time.deltaTime * 7f);
+			renderer.color = Color.Lerp(renderer.color, new Color(1f, 1f, 1f, 0.2f), Time.deltaTime * 7f);
+			yield return null;
+		}
+		transform.localScale = Vector3.one * 0.66f;
+		renderer.color = new Color(1f, 1f, 1f, 0.2f);
+		yield return new WaitForSeconds(waitTime);
+		StartCoroutine(Reappear());
+	}
+
+	IEnumerator Reappear(){
+		GetComponent<Collider2D>().enabled = true;
+		SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+		for (int i = 0; i < countdown.Length; i++){
+			countdown[i].color = new Color(0f, 0f, 0f, 0f);
+		}
+		while (transform.localScale.x < 0.995f){
+			transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one, Time.deltaTime * 7f);
+			renderer.color = Color.Lerp(renderer.color, Color.white, Time.deltaTime * 7f);
+			for (int i = 0; i < countdown.Length; i++){
+				countdown[i].color = Color.Lerp(countdown[i].color, new Color(grey, grey, grey, 1f), Time.deltaTime * 7f);
+			}
+			yield return null;
+		}
+		transform.localScale = Vector3.one;
+		renderer.color = Color.white;
+		for (int i = 0; i < countdown.Length; i++){
+			countdown[i].color = new Color(grey, grey, grey, 1f);
+		}
+		yield return new WaitForSeconds(1f);
+		curTime = 0;
+		AddTime();
+	}
+
+}
